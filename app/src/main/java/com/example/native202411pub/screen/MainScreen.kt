e24fb62b94241c98c30293442fc2ac1eb2a7f389
yono.ms <yono.ms@outlook.com>
2024-07-26 08:27:07 +0900

    refactor.

--- app/src/main/java/com/example/native202411pub/screen/MainScreen.kt
+++ app/src/main/java/com/example/native202411pub/screen/MainScreen.kt
@@ -74,14 +74,6 @@ fun MainScreen(modifier: Modifier = Modifier) {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun MainScreenPreview() {
