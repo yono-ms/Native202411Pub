c3d6f3d0a8733a67de1eb2c1a8128596c23a3311
yono.ap <yono.ap@icloud.com>
2024-07-25 14:55:41 +0900

    plugins.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.plugin.serialization)
+    alias(libs.plugins.google.devtools.ksp)
 }
 
 android {
