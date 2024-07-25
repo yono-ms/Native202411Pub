a93095b2b9d01be46ca59fab52d62d83e8b14972
yono.ms <yono.ms@outlook.com>
2024-07-25 18:27:46 +0900

    slf4j.

--- app/src/main/java/com/example/native202411pub/MainActivity.kt
+++ app/src/main/java/com/example/native202411pub/MainActivity.kt
@@ -12,10 +12,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.native202411pub.ui.theme.Native202411PubTheme
+import org.slf4j.LoggerFactory
 
 class MainActivity : ComponentActivity() {
+
+    private val logger = LoggerFactory.getLogger(MainActivity::class.java)
+
+    private fun loggerTest() {
+        logger.trace("Logger TEST")
+        logger.debug("Logger TEST")
+        logger.info("Logger TEST")
+        logger.warn("Logger TEST")
+        logger.error("Logger TEST")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        loggerTest()
         enableEdgeToEdge()
         setContent {
             Native202411PubTheme {
